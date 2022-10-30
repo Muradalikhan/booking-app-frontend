6211ec24151a69a641c6ab92cc775a7ba7464891
Murad khan <murad.jaddoon@gmail.com>
2022-10-31 00:09:36 +0500

    updated navar conditional

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  useLocation,
+} from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Header from "./components/header/Header";
+import List from "./pages/list/List";
 
 const Layout = () => {
+  const location = useLocation();
+  const listMode = location.pathname === "/list";
   return (
     <>
       <Navbar />
-      <Header />
+      <Header listMode={listMode} />
       <Outlet />
     </>
   );
@@ -23,6 +31,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/list",
+        element: <List />,
       },
     ],
   },
